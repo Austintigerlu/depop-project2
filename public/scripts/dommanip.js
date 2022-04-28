526fc73769d3e0a0cea27a2fc0167824dd91872d
skhreis <skhreis0@gmail.com>
2022-04-28 13:44:54 -0400

    Premature commit that links JS to edit.ejs. Still needs a bit of work.

---
+++ public/scripts/dommanip.js
@@ -0,0 +1,10 @@
+const image = document.querySelector('.edit-image')
+const url = document.querySelector('.url')
+const preview = document.querySelector('.preview')
+
+
+
+preview.addEventListener('click', () => {
+	console.log('hahaxd')
+	image.src = 'https://miro.medium.com/max/1400/0*h8I3MyieMHgR8BYN'
+})
