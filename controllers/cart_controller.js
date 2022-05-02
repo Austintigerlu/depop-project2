8a0043494c42549947a76933df29bc7d5b93441a
skhreis <skhreis0@gmail.com>
2022-05-02 15:13:58 -0400

    Final adjustments #1.

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -9,35 +9,6 @@ router.get('/', (req,res) => {
 });
 
 
-// router.get('/', async (req,res,next) => {
-// 	try {
-// 		const Cart = await db.Cart.find({
-//             cart: 'Product',
-//         })
-// 		const context = { Cart }
-// 		return res.render('./cart/index.ejs', context)
-// 	} catch (error) {
-// 		console.log(error)
-// 		req.error = error;
-// 		return next();
-// 	}
-// })
-
-
-// router.get("/", (req, res) => {
-//     Cart.find({})
-//       .populate("product")
-//       .exec((error, allCart) => {
-//         if (error) {
-//           console.log(error);
-//           req.error = error;
-//           return next();
-//         }
-//         const context = { cart: allCart };
-//         return res.render("./cart/index.ejs", context);
-//      });
-//   });
-
 // -- DELETE ITEMS -- add later
 router.delete('')
 
